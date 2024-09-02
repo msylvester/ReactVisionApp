1245d5d0d451611ce28038a22eb7b7dbf29e5904
mike <msylvest55@gmail.com>
2024-09-02 16:44:28 -0500

    adding constants

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+// constants.js
+
+// Import your image files
+import blue_2x2 from './img/blue_2x2.png';
+import red_2x2 from './img/red_2x2.png';
+import logo from './img/PNG.png';
+
+// Import other images as needed
+import black_2x2 from './img/black_2x2.png';
+import green_2x2 from './img/green_2x2.png';
+import grey_2x2 from './img/grey_2x2.png';
+import folder_img from './img/folder.png';
+//import projecgt icon impage
+const API_KEY = '31b9f8407f4148fb969d92283f45ecc7';
+
+export default imageMap = {
+  blue_2x2,
+  red_2x2,
+  black_2x2,
+  green_2x2,
+  grey_2x2,
+  logo,
+  API_KEY,
+  folder_img,
+  // Add other images to this map
+};
+
+// export const imageFilenames = [
+//   'red_2x2.png',
+//   'blue_2x2.png',
+//   'black_2x2.png',
+//   'green_2x2.png',
+//   'grey_2x2.png',
+// ];
